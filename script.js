e5cd0e354eb6f7ab52948b2d473cc90cfc976186
Kerwin <tkoleo84119@gmail.com>
2022-04-11 22:12:24 +0800

    feat: add keydown event listener

---
+++ script.js
@@ -0,0 +1,18 @@
+function playSound(e) {
+  const element = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!element) return;
+
+  const audio = document.querySelector(
+    `audio[data-key="${element.dataset.key}"]`
+  );
+
+  element.classList.add("playing");
+  audio.currentTime = 0; // reset audio
+  audio.play();
+}
+
+window.addEventListener("keydown", playSound);
+window.addEventListener("transitionend", (e) => {
+  if (e.propertyName !== "transform") return;
+  e.target.classList.remove("playing");
+});
